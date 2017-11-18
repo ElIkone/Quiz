dfae8d129b29b540d7c9393b70880efd0d585227
Ikone <elmaildebenja@gmail.com>
2017-11-17 23:55:41 -0800

    Fix: Refactor question class

--- app/src/main/java/com/example/ben/quiz/Question.java
+++ app/src/main/java/com/example/ben/quiz/Question.java
@@ -1,8 +1,27 @@
 package com.example.ben.quiz;
 
 public class Question {
-    private String mQuestions [] = {
-            "What was the last thing that you fixed?",
-            "What do your clothes say about you?"
-    };
+    public Question(String mQuestion, int id) {
+        this.mQuestion = mQuestion;
+        this.id = id;
+    }
+
+    private String mQuestion;
+    private int id;
+
+    public String getmQuestion() {
+        return mQuestion;
+    }
+
+    public void setmQuestion(String mQuestion) {
+        this.mQuestion = mQuestion;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 }
