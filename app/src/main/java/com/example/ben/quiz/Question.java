f0824b1e1d72a254e663ac92fcfc6337082dbe01
Ikone <elmaildebenja@gmail.com>
2017-11-17 23:38:24 -0800

    Feat: Add Question Class

---
+++ app/src/main/java/com/example/ben/quiz/Question.java
@@ -0,0 +1,8 @@
+package com.example.ben.quiz;
+
+public class Question {
+    private String mQuestions [] = {
+            "What was the last thing that you fixed?",
+            "What do your clothes say about you?"
+    };
+}
